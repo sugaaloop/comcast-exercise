98756932b9b5a881a44b21170aa51b299a597722
Robert Hennessey <rhennessey@kr.af.mil>
2022-04-05 12:19:36 -0400

    init java app

---
+++ app/src/test/java/dev/bobhennessey/comcastexercise/ComcastExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bobhennessey.comcastexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComcastExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
